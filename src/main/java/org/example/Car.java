481d3abcf2e74b52ad8181ab73fb23356ef152cd
Mikhail Maevskiy <57516381+memaev@users.noreply.github.com>
2024-03-06 18:15:53 +0200

    Lesson 06.03

---
+++ src/main/java/org/example/Car.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public interface Car {
+    void drive();
+    void buy();
+    void steal();
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,12 @@
 package org.example;
+import com.sun.source.tree.ClassTree;
 import org.example.entity.*;
 import org.example.repository.RegistrationUtil;
 
+import java.sql.SQLOutput;
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -28,16 +32,38 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        System.gc();
+        LinkedList<String> list = new LinkedList<>();
+        list.add("Str1");
+        list.add("Str2");
+        list.add("Str3"); //добавление элемента в конец списка
+        System.out.println(list);
 
-        Teacher teacher = new Teacher(30, 5, "Teacher", "One");
-        RegistrationUtil.register(teacher);
-        System.out.println(teacher.getAge());
+        //удаление первого появления элемента в списке
+        list.remove("Str3");
+        System.out.println(list);
 
-        ArrayList<String> arr = new ArrayList<>();
-        arr.add("Jnn");
-        arr.add("pop");
+        //добавление элемента в начало списка
+        list.addFirst("Str4");
+        System.out.println(list);
 
-        arr.remove("pop");
+        System.out.println("Enter line: ");
+        String str = scan.nextLine(); //считываем данные от пользователя
+        if (list.contains(str)){
+            System.out.println("List contains input str");
+        }else{
+            System.out.println("List does not contain input str");
+        }
+
+        if (list.isEmpty()){
+            System.out.println("List is empty");
+        }else
+            System.out.println("List is not empty");
+
+        for (String string : list){
+            System.out.println(string);
+        }
+
+        List<Double> arrList = new ArrayList<>();
+
     }
 }
